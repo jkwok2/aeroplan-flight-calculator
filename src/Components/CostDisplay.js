d219ec2c176a6f7466abfaca6f74d7447389eb20
imogenem <mcdonaldimo1@gmail.com>
2021-01-10 02:50:00 -0800

    recent update

--- src/Components/CostDisplay.js
+++ src/Components/CostDisplay.js
@@ -4,29 +4,38 @@ import Calculator from './Calculator'
 
 class CostDisplay extends Component {
   
-    // constructor (inputString, cos) {
-    //     this.inputString = inputString
-    //     this.cos = cos
-    //     this.cityArr = []
-    //     this.distances = []
-    //     this.originZone;
-    //     this.destZone;
-    // }
+    constructor () {
+        super();
+        var calc = new Calculator();
+        
+        this.publish = this.publish.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+      }
+      
+      handleChange({ target }) {
+        this.setState({
+          [target.name]: target.value
+        });
+      }
+    
+      publish() {
+        console.log( this.state.topicBox, this.state.payloadBox );
+      }
 
-    var calc = new Calculator();
+    
 
     render(){
         return (  // every component needs a render
            
-            //<TextField id="outlined-basic" label="Trip Cost" variant="outlined" />
+            <TextField id="outlined-basic" label="Trip Cost" variant="outlined" />
        
-            <TextField
-            id="outlined-basic"
-            label="Trip Cost"
+        //     <TextField
+        //     id="outlined-basic"
+        //     label="Trip Cost"
     
-            }}
-            onChange={(e) => setValue(calc.totalPrice(zone, band, cos))}
-          />
+        //     }}
+        //     onChange={(e) => setValue(calc.totalPrice(zone, band, cos))}
+        //   />
           
         )
     }
