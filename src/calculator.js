089bbc59c61c46b125aefbd92bc7e1a40f140b2f
Jeffrey Kwok <47469240+jkwok2@users.noreply.github.com>
2021-01-10 00:12:45 -0800

    fixed bugs and working print statements

--- src/calculator.js
+++ src/calculator.js
@@ -21,13 +21,14 @@ class Calculator {
         this.destZone;
 
     }
-// let inputString = "YVR-YYZ-ZRH"; // from form;
-// let cos = "j" // from form
-// let cityArr = [];
-// let distances = [];
-// let originZone; 
-// let destZone;
 }
+
+let inputString = "YVR-YYZ-ZRH"; // from form;
+let cos = "j" // from form
+let cityArr = [];
+let distances = [];
+let originZone; 
+let destZone;
 
 const splitString = (inputString) => {
     cityArr = inputString.split("-");
@@ -73,24 +74,24 @@ const distInArray = (cityArr) => {
 const calcRegion = (country) => {
     if (countries[country] === "North America") {
         return "NA"
-    } else if (continent === "Europe") {
+    } else if (countries[country] === "Europe") {
         return "AT"
-    } else if (continent === "Africa") {
+    } else if (countries[country] === "Africa") {
         return "AT"
-    } else if (continent === "South America") {
+    } else if (countries[country] === "South America") {
         return "SA"
-    } else if (continent === "Oceania") {
+    } else if (countries[country] === "Oceania") {
         return "PA"
-    } else if (continent === "Asia") {
+    } else if (countries[country] === "Asia") {
         return "PA"
-    } else if (continent === "Atlantic") {
+    } else if (countries[country] === "Atlantic") {
         return "AT"
     }
 }
 
 const calcZone = (cityArr) => {
     let originCountry = airportData[cityArr[0]].location;
-    let destCountry = airportData[cityArr.length].location;
+    let destCountry = airportData[cityArr[cityArr.length - 1]].location
     let originZone = calcRegion(originCountry);
     let destZone = calcRegion(destCountry);
     return originZone + destZone
@@ -100,89 +101,89 @@ const distToBand = (zone, dist) => {
     switch (zone) {
         case "ATAT":
             if (0 <= dist && dist <= 1000) {
-                return band1
+                return "band1"
             } else if (1001 <= dist && dist <= 2000) {
-                return band2
+                return "band2"
             } else if (2001 <= dist && dist <= 4000) {
-                return band3
+                return "band3"
             } else if (4001 <= dist && dist <= 6000) {
-                return band4
+                return "band4"
             } else if (6001 <= dist) {
-                return band5
+                return "band5"
             } 
         case "ATPA":
             if (0 <= dist && dist <= 5000) {
-                return band1
+                return "band1"
             } else if (5001 <= dist && dist <= 7000) {
-                return band2
+                return "band2"
             } else if (7001 <= dist) {
-                return band3
+                return "band3"
             }
         case "ATSA":
             if (0 <= dist && dist <= 7000) {
-                return band1
+                return "band1"
             } else if (7001 <= dist) {
-                return band2
+                return "band2"
             }
         case "NAAT":
             if (0 <= dist && dist <= 4000) {
-                return band1
+                return "band1"
             } else if (4001 <= dist && dist <= 6000) {
-                return band2
+                return "band2"
             } else if (6001 <= dist && dist <= 8000) {
-                return band3;
+                return "band3";
             } else if (8001 <= dist){
-                return band4;
+                return "band4";
             }
         case "NANA":
             if (0 <= dist && dist <= 500) {
-                return band1;
+                return "band1";
             } else if (501 <= dist && dist <= 1500){
-                return band2;
+                return "band2";
             } else if (1501 <= dist && dist <= 2750) {
-                return band3;
+                return "band3";
         
             } else if (2751 <= dist) {
-                return band4;
+                return "band4";
             }
         case "NAPA":
             if (0 <= dist && dist <= 5000) {
-                return band1;
+                return "band1";
             } else if (5001 <= dist && dist <= 7500) {
-                return band2;
+                return "band2";
             } else if (7501 <= dist && dist <= 11000) {
-                return band3;
+                return "band3";
             } else if (11001 <= dist) {
-                return band4;
+                return "band4";
             }
 
         case "NASA":
             if (0 <= dist && dist <= 4500) {
-                return band1;
+                return "band1";
             } else if (4501 <= dist) {
-                return band2;
+                return "band2";
             }
         
         case "PAPA":
             if (0 <= dist && dist <= 1000) {
-                return band1
+                return "band1"
             } else if (1001 <= dist && dist <= 2000) {
-                return band2
+                return "band2"
             } else if (2001 <= dist && dist <= 5000) {
-                return band3
+                return "band3"
             } else if (5001 <= dist && dist <= 7000) {
-                return band4
+                return "band4"
             } else if (7000 <= dist) {
-                return band5
+                return "band5"
             } 
         case "PASA":
             if (0 <= dist && dist <= 11000) {
-                return band1
+                return "band1"
             } else if (11000 <= dist) {
-                return band2
+                return "band2"
             }
         case "SASA":
-            return band1
+            return "band1"
         
     }
 }
@@ -190,86 +191,86 @@ const distToBand = (zone, dist) => {
 const totalPrice = (zone, band, cos) => {
     switch (zone) {
         case "ATAT":
-            if (cos === e) {
+            if (cos === "e") {
                 return ATAT[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return ATAT[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return ATAT[band].first
             }
         case "ATPA":
-            if (cos === e) {
+            if (cos === "e") {
                 return ATAT[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return ATAT[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return ATAT[band].first
             }
         case "ATSA":
-            if (cos === e) {
+            if (cos === "e") {
                 return ASAS[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return ASAS[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return ASAS[band].first
             }
         case "NAAT":
-            if (cos === e) {
+            if (cos === "e") {
                 return NAAT[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return NAAT[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return NAAT[band].first
             }
         case "NANA":
-            if (cos === e) {
+            if (cos === "e") {
                 return NANA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return NANA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return NANA[band].first
             }
         case "NAPA":
-            if (cos === e) {
+            if (cos === "e") {
                 return NAPA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return NAPA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return NAPA[band].first
             }
         case "NASA":
-            if (cos === e) {
+            if (cos === "e") {
                 return NASA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return NASA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return NASA[band].first
             }        
         case "PAPA":
-            if (cos === e) {
+            if (cos === "e") {
                 return PAPA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return PAPA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return PAPA[band].first
             }
         case "PASA":
-            if (cos === e) {
+            if (cos === "e") {
                 return PASA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return PASA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return PASA[band].first
             }
         case "SASA":
-            if (cos === e) {
+            if (cos === "e") {
                 return SASA[band].economy
-            } else if (cos == j) {
+            } else if (cos == "j") {
                 return SASA[band].business
-            } else if (cos == f) {
+            } else if (cos == "f") {
                 return SASA[band].first
             }
-        
+    }
 }
 
 // yvr-yyz-zrh
@@ -277,10 +278,24 @@ const totalPrice = (zone, band, cos) => {
 // [2079, 4033]
 // [5000]
 
-let testinputString = "YVR-YYZ-ZRH";
-let testCityArr = []
-splitString(testinputString)
-distInArray(testCityArr);
-console.log(testcityArr)
+// let testinputString = "YVR-YYZ-ZRH";
+// let testCityArr = []
+// distInArray(cityArr);
+// splitString(testinputString)
+// distInArray(testCityArr);
+// console.log(testcityArr)
+// console.log(distances)
+// console.log(totalDistance(distances))
+
+splitString(inputString)
+console.log(cityArr)
+distInArray(cityArr)
 console.log(distances)
-console.log(totalDistance(distances)) // DISPLAY
+testTotalDistance = totalDistance(distances)
+console.log(testTotalDistance)
+var testZone = calcZone(cityArr)
+console.log(testZone)
+var testBand = distToBand(testZone, testTotalDistance)
+console.log(testBand)
+var testTotalPrice = totalPrice(testZone, testBand, "j")
+console.log(testTotalPrice)
