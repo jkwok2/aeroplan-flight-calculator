4b0f163a5e3a408c85007ac2eac70ef4134ba889
imogenem <mcdonaldimo1@gmail.com>
2021-01-10 11:30:25 -0800

    updating master

--- src/Components/ClassDropdown.js
+++ src/Components/ClassDropdown.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import Calculator from './Calculator'
+
 
 class ClassDropdown extends Component {
   
--- src/Components/CostDisplay.js
+++ src/Components/CostDisplay.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react'
 import TextField from '@material-ui/core/TextField';
-import Calculator from './Calculator'
+
 
 class CostDisplay extends Component {
   
-    constructor (props) {
-        super();
-        this.state = {
-          data : this.props.dataParentToChild
-        }
+    // constructor (props) {
+        // super();
+        // this.state = {
+        //   data : this.props.dataParentToChild
+        // }
         // instantiate Calculator:
         // var calc = new Calculator();
       
-      render() {
-
-        const {data} = this.state;
-
+      render(){
         return (
 
-          <div>
-            {data}
-          </div>
+          <TextField id="outlined-basic" label="Trip Cost" variant="outlined" />
+
+        //  const {data} = this.state;
+
+        //   <div>
+        //     {data}
+        //   </div>
 
           // Code from tutorial on outputting text from a textfield
           // <React.Fragment>
@@ -40,7 +41,7 @@ export default CostDisplay;
      // Code from tutorial on outputting text from a textfield
         // this.publish = this.publish.bind(this);
         // this.handleChange = this.handleChange.bind(this);
-      }
+      // }
     
       // Code from tutorial on outputting text from a textfield
       // handleChange = event => {
