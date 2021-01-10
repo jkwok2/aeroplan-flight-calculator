230a34c26a34e408e14bfd39e45bccc926482e0a
imogenem <mcdonaldimo1@gmail.com>
2021-01-10 02:24:34 -0800

    moved calculator

--- src/Components/CostDisplay.js
+++ src/Components/CostDisplay.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react'
 import TextField from '@material-ui/core/TextField';
+import Calculator from './Calculator'
 
 class CostDisplay extends Component {
   
+    // constructor (inputString, cos) {
+    //     this.inputString = inputString
+    //     this.cos = cos
+    //     this.cityArr = []
+    //     this.distances = []
+    //     this.originZone;
+    //     this.destZone;
+    // }
+
+    var calc = new Calculator();
 
     render(){
         return (  // every component needs a render
            
-            <TextField id="outlined-basic" label="Trip Cost" variant="outlined" />
+            //<TextField id="outlined-basic" label="Trip Cost" variant="outlined" />
        
+            <TextField
+            id="outlined-basic"
+            label="Trip Cost"
+    
+            }}
+            onChange={(e) => setValue(calc.totalPrice(zone, band, cos))}
+          />
           
         )
     }
--- src/Components/InputTextField.js
+++ src/Components/InputTextField.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Calculator from './Calculator'
 
 
 class InputTextField extends React.Component {
